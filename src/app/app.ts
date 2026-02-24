12a8545677042b352b56bec1f70bf893544f118a
Ravish.Varma <ravish2000kumar@gmail.com>
2026-02-24 23:27:35 +0530

    Using signal with counter

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, OnInit, Signal, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,29 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('signal');
+export class App implements OnInit{
+
+  constructor(){
+        effect(()=>{
+      console.log("effect running after signal update: ",this.counter());
+    });
+    effect(()=>{
+      console.log("sum : ", this.c());
+      
+    })
+  }
+  ngOnInit(): void {
+
+  }
+  // protected readonly title = signal('signal');
+  counter = signal(0);
+
+  a:WritableSignal<number> = signal(0);
+  b:WritableSignal<number> = signal(0);
+  c:Signal<number> = computed(()=> this.a() + this.b());
+
+  increase(event:any){
+    this.counter.update((val)=> val+1);
+    this.a.set(this.counter())
+  }
 }
